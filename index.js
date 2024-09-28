23b2e2fbc4001fb23bab78b00a32a1c684cbc9d0
BlackJackYan1 <mochamedandryan@gmail.com>
2024-09-28 17:46:57 +0700

    miyan

--- index.js
+++ index.js
@@ -1,57 +1,34 @@
-const { Client, RichPresence } = require('discord.js-selfbot-v13');
-const client = new Client();
-const { exec, spawn } = require("child_process")
 const express = require('express');
 const app = express();
+const { exec } = require('child_process');
 
-function handleGit() {
-exec("git reset --hard HEAD && git pull")
-const child = spawn(process.argv[0], process.argv.slice(1), {
-    detached: true,
-    stdio: 'inherit'
-  });
-  child.unref();
-  process.exit();
-}
+let miyanrpc = exec('node rpc.js');
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.status(200).send('Hello, World');
 });
 
 app.get('/push', (req, res) => {
-  res.status(200).send('Success Pulling');
-  handleGit();
+  const token = req.headers['x-hub-signature'];
+  if (token === 'MiyanRPC') {
+    exec('sh pull.sh', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error during pull: ${error}`);
+        res.status(500).send('Error during pull');
+        return;
+      }
+      // Stop the current rpc.js process
+      miyanrpc.kill();
+
+      // Restart rpc.js
+      miyanrpc = exec('node rpc.js');
+      res.status(200).send('Pulled latest changes and restarted rpc.js');
+    });
+  } else {
+    res.status(403).send('Invalid token');
+  }
 });
 
-app.use((req, res) => {
-  res.status(404).send('Not Found');
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
 });
-
-app.listen(5000)
-
-client.on('ready', async () => {
-  console.clear()
-  console.log(`${client.user.username} is ready!`);
-  const getExtendURL = await RichPresence.getExternal(
-    client,
-    process.env.APPID,
-    'https://files.catbox.moe/nk86rq.jpg' // Required if the image you use is not in Discord
-  );
-  const status = new RichPresence(client)
-    .setApplicationId(process.env.APPID)
-    .setType('WATCHING')
-    // .setURL('https://www.youtube.com/watch?v=5icFcPkVzMg') // If you set a URL, it will automatically change to STREAMING type
-    .setState('She Is Mine.')
-    .setName('Someone Who Will Be Mine.')
-    .setDetails('Ruan Mei')
-    .setStartTimestamp(Date.now())
-    .setAssetsLargeImage(getExtendURL[0].external_asset_path) // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
-    // .setAssetsLargeText('Idle')
-    // .setAssetsSmallImage('373370493127884800') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
-    // .setAssetsSmallText('click the circles')
-    // .setPlatform('desktop')
-     .addButton('Youtube', 'https://m.youtube.com/@Miyan0001');
-  client.user.setPresence({activities: [status], status: 'online'});
-});
-
-client.login(process.env.TOKEN);
---
+++ rpc.js
@@ -0,0 +1,29 @@
+const { Client, RichPresence } = require('discord.js-selfbot-v13');
+const client = new Client();
+
+client.on('ready', async () => {
+  console.clear()
+  console.log(`${client.user.username} is ready!`);
+  const getExtendURL = await RichPresence.getExternal(
+    client,
+    process.env.APPID,
+    'https://files.catbox.moe/nk86rq.jpg' // Required if the image you use is not in Discord
+  );
+  const status = new RichPresence(client)
+    .setApplicationId(process.env.APPID)
+    .setType('WATCHING')
+    // .setURL('https://www.youtube.com/watch?v=5icFcPkVzMg') // If you set a URL, it will automatically change to STREAMING type
+    .setState('She Is Mine.')
+    .setName('Someone Who Will Be Mine.')
+    .setDetails('Ruan Mei')
+    .setStartTimestamp(Date.now())
+    .setAssetsLargeImage(getExtendURL[0].external_asset_path) // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
+    // .setAssetsLargeText('Idle')
+    // .setAssetsSmallImage('373370493127884800') // https://discord.com/api/v9/oauth2/applications/367827983903490050/assets
+    // .setAssetsSmallText('click the circles')
+    // .setPlatform('desktop')
+     .addButton('Youtube', 'https://m.youtube.com/@Miyan0001');
+  client.user.setPresence({activities: [status], status: 'online'});
+});
+
+client.login(process.env.TOKEN);
