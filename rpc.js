f298206aedb56c89f20a44e23d636b267347f39b
BlackJackYan1 <mochamedandryan@gmail.com>
2024-09-29 01:35:30 +0700

    miyan

--- rpc.js
+++ rpc.js
@@ -4,21 +4,21 @@ const client = new Client();
 client.on('ready', async () => {
   console.clear()
   console.log(`${client.user.username} is ready!`);
-  client.user.setSamsungActivity('com.HoYoverse.hkrpgoversea', 'START');
-  // const getExtendURL = await RichPresence.getExternal(
-    // client,
-    // process.env.APPID,
-    // 'https://files.catbox.moe/nk86rq.jpg' // Required if the image you use is not in Discord
-  // );
-  // const status = new RichPresence(client)
-    // .setApplicationId(process.env.APPID)
-    // .setType('WATCHING')
-    // .setState('She Is Mine.')
-    // .setName('Someone Who Will Be Mine.')
-    // .setDetails('Ruan Mei')
-    // .setStartTimestamp(Date.now())
-    // .setAssetsLargeImage(getExtendURL[0].external_asset_path) // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
-  // client.user.setPresence({activities: [status], status: 'online'});
+  const getExtendURL = await RichPresence.getExternal(
+    client,
+    process.env.APPID,
+    'https://files.catbox.moe/nk86rq.jpg',
+    'https://files.catbox.moe/kjkbdd.png'
+  );
+  const status = new RichPresence(client)
+    .setApplicationId(process.env.APPID)
+    .setType('PLAYING')
+   // .setState('She Is Mine.')
+    .setName('Honkai: Star Rail')
+   // .setDetails('')
+    .setStartTimestamp(Date.now() - 24 * 60 * 60 * 1000)
+    .setAssetsLargeImage(getExtendURL[1].external_asset_path) // https://assets.ppy.sh/beatmaps/1550633/covers/list.jpg
+  client.user.setPresence({activities: [status], status: 'online'});
 });
 
 client.login(process.env.TOKEN);
